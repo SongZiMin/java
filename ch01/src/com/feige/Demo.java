0291369c2a51886d074f177544c3a2905904b03d
宋梓民 <74894907+SongZiMin@users.noreply.github.com>
2021-09-16 17:57:15 +0800

    Add files via upload

---
+++ ch01/src/com/feige/Demo.java
@@ -0,0 +1,17 @@
+package com.feige;
+
+import java.util.Scanner;
+
+/*
+* 启动类
+*
+ */
+public class Demo {
+    public static void main(String[] args) {
+     new Scanner(System.in);
+     //int [] a = new int[2] {1,3,3};
+
+
+
+    }
+}
